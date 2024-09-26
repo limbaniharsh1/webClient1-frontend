64d823bd161ff1a11cc0535eb3e6ab7af3f3d311
harsh limbani <limbaniharsh1@gmail.com>
2024-09-26 23:28:36 +0530

    fix scroll issue, cause of animation

--- src/pages/client/home/ServicesSection.jsx
+++ src/pages/client/home/ServicesSection.jsx
@@ -27,16 +27,15 @@ const ServicesSection = () => {
     <section>
       <div className="container paddingBottom paddingTop-56">
         <Col sm={12} lg={10} className="mx-auto">
-          <Row>
+          <Row className="overflow-hidden">
             {data?.map((item, index) => {
               return (
                 <Col
                   sm={12}
                   md={6}
                   lg={4}
-                  className="pt-4 responsive"
+                  className="pt-4 responsive overflow-hidden"
                   data-aos="fade-left"
-                  data-aos-delay={300 * index}
                 >
                   <div className="box-shadow-lg rounded-3 p-4 banner-service-hover-card">
                     <span className="home-service-icon d-block rounded-3 text-color-white d-flex align-items-center justify-content-center text-start fs-28">
@@ -45,7 +44,7 @@ const ServicesSection = () => {
                     <h3 className="fs-20 mt-4 ff-primary text-truncate text-color-primary">
                       {item.title}
                     </h3>
-                    <p className="fs-13  truncate-line-3 text-color-gray">
+                    <p className="fs-14 truncate-line-3 text-color-gray">
                       {item.description}
                     </p>
                   </div>
