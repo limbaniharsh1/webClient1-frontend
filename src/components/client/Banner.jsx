3bf01b3ac04d2346cd627702006bda36ef0ec2a5
harsh limbani <limbaniharsh1@gmail.com>
2024-10-04 22:57:22 +0530

    create participation and about us page

--- src/components/client/Banner.jsx
+++ src/components/client/Banner.jsx
@@ -3,10 +3,12 @@ import React from "react";
 const Banner = ({ title }) => {
   return (
     <section>
-      <div className="common-bg d-flex align-items-center justify-content-center">
-        <h2 className="ff-primary fs-48 fw-bold mb-2 mb-md-3 main-banner-title text-color-white text-uppercase">
-          {title}
-        </h2>
+      <div className="common-bg d-flex align-items-center text-center">
+        <div className="container">
+          <h2 className="ff-primary fs-48 fw-bold mb-2 mb-md-3 main-banner-title text-color-white text-uppercase">
+            {title}
+          </h2>
+        </div>
       </div>
     </section>
   );
