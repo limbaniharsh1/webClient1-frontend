3153739d7f959623554d7daa0a6a55b43ab26634
harsh limbani <limbaniharsh1@gmail.com>
2024-12-11 20:17:48 +0530

    changes

--- src/pages/client/Layout.jsx
+++ src/pages/client/Layout.jsx
@@ -4,10 +4,9 @@ import Footer from "../../components/client/Footer";
 import { mobile } from "../../enum";
 
 const Layout = ({ children }) => {
-
   return (
     <main>
-      <Header />
+      {/* <Header /> */}
       {children}
       <a href={`https://wa.me/${mobile}`} target="blank">
         <span className="fix-whatsapp-icon d-flex align-items-center justify-content-center">
